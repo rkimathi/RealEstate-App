e313ce1ed5d01fb82a29a9b69b995f7a35771145
Mwika Roy <mwika.roy@technoincor.com>
2025-06-18 00:40:44 +0300

    First COmmit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App(props) {
+    return (
+        <div className="text-red-500">
+            <h1>Hello Africa</h1>
+
+        </div>
+    );
+}
+
+export default App;
